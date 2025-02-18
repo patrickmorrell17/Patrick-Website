9b767e6b645f4d28f7b43c79f8bdfb0bd7a2e088
Patrick Morrell <patrick.morrell@wsu.edu>
2025-02-18 10:45:02 -0600

    Visual touch ups for tabs

--- patrick-website/src/app/page.tsx
+++ patrick-website/src/app/page.tsx
@@ -1,18 +1,24 @@
 'use client';
 import { Tabs } from "@mantine/core";
+import { useState } from 'react';
 import { IconHome,IconMail,IconBrandGithub} from '@tabler/icons-react';
+import tabStyle from './tabStyle.module.css';
 
 export default function Home() {
+  const [activeTab, setActiveTab] = useState<string | null>('first');
+
   return (
-        <Tabs color="blue" defaultValue="gallery" orientation="vertical">
-          <Tabs.List>
-            <Tabs.Tab value="Home" leftSection={<IconHome size={12}/>}>Home</Tabs.Tab>
-            <Tabs.Tab value="Projects" leftSection={<IconBrandGithub size={12}/>}>Projects and Skills</Tabs.Tab>
-            <Tabs.Tab value="Contact" leftSection={<IconMail size={12}/>}>Contact</Tabs.Tab>
+    <div>
+        <Tabs value={activeTab} onChange={setActiveTab}  color="blue" orientation="vertical" classNames={tabStyle}>
+          <Tabs.List className={tabStyle.tabDiv}>
+            <Tabs.Tab  value="Home" leftSection={<IconHome size={30}/>}>Home</Tabs.Tab>
+            <Tabs.Tab value="Projects" leftSection={<IconBrandGithub size={30}/>}>Projects and Skills</Tabs.Tab>
+            <Tabs.Tab value="Contact" leftSection={<IconMail size={30}/>}>Contact</Tabs.Tab>
           </Tabs.List>
           <Tabs.Panel value="Home">Gallery tab content</Tabs.Panel>
           <Tabs.Panel value="Projects">Messages tab content</Tabs.Panel>
           <Tabs.Panel value="Contact">Settings tab content</Tabs.Panel>
         </Tabs>
+    </div>
   );
 }
