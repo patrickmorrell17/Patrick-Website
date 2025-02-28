626fbc2e8facbe920143e97a874777cd7de8bb02
Patrick Morrell <patrick.morrell@wsu.edu>
2025-02-28 15:12:43 -0600

    Move static text to JSON

--- patrick-website/src/app/welcomeTab.tsx
+++ patrick-website/src/app/welcomeTab.tsx
@@ -1,11 +1,12 @@
 'use client';
 import {Title, Image, Text} from "@mantine/core";
 import welcomeTab from "./welcomeTab.module.css";
+import staticText from "../../public/staticText.json";
 
 export default function WelcomeTab() {
   return (
     <>
-        <Title className={welcomeTab.title} order={1}>Patrick Morrell Full Stack Web Developer</Title>
+        <Title className={welcomeTab.title} order={1}>{staticText.title}</Title>
         <div className={welcomeTab.picture}>
             <Image h={350}
                    w="auto"
@@ -14,12 +15,8 @@ export default function WelcomeTab() {
             />
         </div>
         <div className={welcomeTab.details}>
-          <Title order={3}>Welcome to My Website</Title>
-          <Text size="md">Thank you for visiting my website! 
-            I’m a results-driven and innovative full stack Software Developer with 3+ years experience in designing, developing, and maintaining innovative software solutions. 
-            Skilled in problem-solving, debugging, and optimizing code to enhance performance and user experience. If you’re interested in my resume, you can download it below! 
-            If you want to reach out to me see the contact tab - doesn’t have to be about a job:)  
-            Always looking to chat with other folks in the industry and discuss cool projects.</Text>
+          <Title order={3}>{staticText.header}</Title>
+          <Text size="md">{staticText.introPara}</Text>
         </div>
     </>
   );
